7d3d030608a0894bef28a5fb1d10859b58e98cf2
Ahmed Zribi <ahmedzribiahmed@gmail.com>
2023-04-26 01:59:36 +0100

    Use remix Form component in order to disable full page reload on form submit

--- app/components/CreateNote.tsx
+++ app/components/CreateNote.tsx
@@ -1,14 +1,15 @@
 import type { LinksFunction } from '@remix-run/node'
+import { Form } from '@remix-run/react'
 import styles from  './CreateNote.css'
 
 function CreateNote() {
   return (
     <div>
-        <form method='post' className='create-note-form'>
+        <Form method='post' className='create-note-form'>
             <input type="text" name='title' placeholder='Title ...' />
             <textarea name="content" cols={30} rows={10} placeholder='Content ...'></textarea>
             <button type='submit'>Add</button>
-        </form>
+        </Form>
     </div>
   )
 }
